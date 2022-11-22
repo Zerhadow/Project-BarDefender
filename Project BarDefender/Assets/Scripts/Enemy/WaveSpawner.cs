9720188d1f975c747f5aa06cd10fe6bb5e5c669c
Julian <jrg180004@utdallas.edu>
2022-11-22 13:15:42 -0600

    started wave spawner

---
+++ Project BarDefender/Assets/Scripts/Enemy/WaveSpawner.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState {SPAWNING, WAITING, COUNTING};
+
+    [System.Serializable]
+    public class Wave {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start() {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update() {
+        if(waveCountdown <= 0) {
+            if(state != SpawnState.SPAWNING) {
+                //start spawning wave
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        } else {
+            waveCountdown -=Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave _wave) {
+        state = SpawnState.SPAWNING;
+
+        for(int i = 0; i < _wave.count; i++) {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1/_wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy) {
+        //spawn enemy
+        Debug.Log("Spawning Enemy: " + _enemy.name);
+    }
+}
