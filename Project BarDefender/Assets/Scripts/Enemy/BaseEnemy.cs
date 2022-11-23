3d06731d0fc3e4d92bce9f35b8ec8816547c3b76
Samuel Chandler <sec180005@utdallas.edu>
2022-11-22 19:46:13 -0600

    Revert "Started Inventory System"

--- Project BarDefender/Assets/Scripts/Enemy/BaseEnemy.cs
+++ Project BarDefender/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnemy : Units
+public class BaseEnemy : MonoBehaviour
 {
-    public override void Die() {
-        base.Die();
-        // AudioManager.PlaySound("EnemyDeath");
-        Destroy(this.gameObject);
-        //drop ingredient function based on monster rarity
-        Debug.Log(transform.name + " died");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
