c5a14f791cdf3a3d531a70ddb1d565cf0854477b
Brandon Hutson <nyancatdoge@gmail.com>
2022-11-27 13:57:07 -0600

    Update Footstep_Script.cs

--- Project BarDefender/Assets/Audio/SFX/Footstep_Script.cs
+++ Project BarDefender/Assets/Audio/SFX/Footstep_Script.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Footstep_Script : PlayerController
+
+public class Footstep_Script : MonoBehaviour
 {
     public AudioSource footStepSound;
 
-    private bool GetIsGrounded()
-    {
-        return isGrounded;
-    }
+    
+    
 
-    void Update(bool isGrounded)
+    
+
+    void Update()
     {
-        footStepSound.enabled = (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && isGrounded == true;
+        footStepSound.enabled = (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && PlayerController.isGrounded == true;
+
     }
 }
